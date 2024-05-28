2bfec2bf538e330b6d1907f4cf440de154914f61
GabrielCosta000 <gabcosgon@gmail.com>
2024-05-28 14:51:25 -0300

    Commit02

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,7 +3,13 @@
         <div class="container-lg">
             <div class="row">
                 <div class="col-sm-4 col-lg-4 m-4">
-                    <a href="/admin/product" class="btn btn-primary">Produtos</a>
+                    <div class="card bg-secondary">
+                        <div class="card-body">
+                            <h5 class="card-title text-light">Produtos</h5>
+                            <p class="card-text text-light">Gerencie seus produtos aqui</p>
+                            <a href="/admin/product" class="btn btn-primary">Produtos</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
